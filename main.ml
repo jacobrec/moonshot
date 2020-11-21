eedaf7d401340256334c8a1e03dc51b9754f9b61
Jacob Reckhard <jacobreckhard@gmail.com>
2020-11-21 13:47:32 -0700

    audio works

--- main.ml
+++ main.ml
@@ -2,15 +2,21 @@ open Raylib
 open Moonshot
 
 
-
+let music = load_music_stream "sound/background.mp3"
 let setup () =
   init_window screen_width screen_height "Starshot";
+  init_audio_device ();
   set_target_fps 60;
+  play_music_stream music;
   Model.MenuScreen
 
+let cleanup () =
+  close_audio_device ();
+  close_window ()
 
 let rec loop model =
-  if window_should_close () then close_window () else
+  update_music_stream music;
+  if window_should_close () then cleanup () else
     model
     |> Input.input
     |> Update.update (get_frame_time ())
